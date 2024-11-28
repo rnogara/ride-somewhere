605e626db0078bc5045a414a2c85c0056d6b7dd9
rnogara <r.nogara.dev@gmail.com>
2024-11-27 21:29:18 -0300

    minor fixes

--- backend/src/ride/ride.module.ts
+++ backend/src/ride/ride.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { RideService } from './ride.service';
 import { RideController } from './ride.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Ride } from 'src/database/entities/ride.entity';
+import { Ride } from '../database/entities/ride.entity';
 import { HttpModule } from '@nestjs/axios';
-import { DriversModule } from 'src/drivers/drivers.module';
+import { DriversModule } from '../drivers/drivers.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Ride]), HttpModule, DriversModule],
